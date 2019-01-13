ab5fb9df12346873a6e2cef5637f88946bba757a
Flzzyf <2981019122@qq.com>
2019-01-13 16:13:31 +0800

    test

---
+++ Heroes of Mighty and Magic and the Storm/Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Profiler.BeginSample("zyf");
+
+		print("qwe");
+
+		Profiler.EndSample();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Profiler.BeginSample("zyf");
+
+		//print("qwe");
+		print(Camera.main.transform.position);
+
+		Profiler.EndSample();
+	}
+}
